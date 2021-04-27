939a45024287af7d543c0d1309a32d59314253d5
Bor Sijbrandij <bor.sijbrandij@gmail.com>
2021-04-27 19:38:55 +0200

    first commit, created smart contract

---
+++ Solidity/Lottery.sol
@@ -0,0 +1,13 @@
+
+pragma solidity >=0.7.0 <0.9.0;
+ 
+ 
+contract Lottery{
+    
+    struct participant{
+        uint amount_payed;
+        address p_adress;
+        bool winner;
+        
+    }
+}
